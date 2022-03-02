2e2303e85fa35e01f5cac0aba3f0af398fe7d0d2
vakhairulislam <tubebusy@gmail.com>
2022-03-02 18:09:49 +0600

    Function Created for Toogle and searchfield

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// Spinner Toggle 
+
+const toggleSpinner = displayStyle => {
+    document.getElementById('spinner').style.display = displayStyle;
+}
+
+
+const searchPhone = () => {
+    const searchField = document.getElementById('search-field');
+
+    // Error Handling 
+
+    const searchText = searchField.value;
+    console.log(searchText);
+
+
+}
